76c1fdf6763fff9f4cd4416f4d62045b65c3286e
Dat PHAM HOANG <dphamhoang@linagora.com>
2025-06-05 18:40:08 +0700

    tw-2349 Hide deleted message in preview of Chat list

--- lib/domain/model/room/room_extension.dart
+++ lib/domain/model/room/room_extension.dart
@@ -1,6 +1,12 @@
+import 'dart:convert';
+
+import 'package:fluffychat/config/app_config.dart';
 import 'package:fluffychat/domain/model/search/recent_chat_model.dart';
 import 'package:fluffychat/utils/matrix_sdk_extensions/client_stories_extension.dart';
+import 'package:fluffychat/utils/matrix_sdk_extensions/matrix_locals.dart';
+import 'package:flutter/material.dart';
 import 'package:matrix/matrix.dart';
+import 'package:flutter_gen/gen_l10n/l10n.dart';
 
 extension RoomExtension on Room {
   RecentChatSearchModel toRecentChatSearchModel(
@@ -147,5 +153,111 @@ extension RoomExtension on Room {
   bool get canRedactEventSentByOther {
     if (!canSendRedactEvent) return false;
     return canRedact;
+  }
+
+  Future<Event?> lastEventAvailableInPreview(BuildContext context) async {
+    // as lastEvent calculation is based on the state events we unfortunately cannot
+    // use sortOrder here: With many state events we just know which ones are the
+    // newest ones, without knowing in which order they actually happened. As such,
+    // using the origin_server_ts is the best guess for this algorithm. While not
+    // perfect, it is only used for the room preview in the room list and sorting
+    // said room list, so it should be good enough.
+    Event? lastEventAvailableInPreview;
+    try {
+      final lastEvents =
+          client.roomPreviewLastEvents.map(getState).whereType<Event>();
+
+      lastEventAvailableInPreview = lastEvents.isEmpty
+          ? null
+          : lastEvents.reduce((a, b) {
+              if (AppConfig.hideRedactedEvents) {
+                if (a.redacted) return b;
+                if (b.redacted) return a;
+              }
+              if (a.originServerTs == b.originServerTs) {
+                // if two events have the same sort order we want to give encrypted events a lower priority
+                // This is so that if the same event exists in the state both encrypted *and* unencrypted,
+                // the unencrypted one is picked
+                return a.type == EventTypes.Encrypted ? b : a;
+              }
+              return a.originServerTs.millisecondsSinceEpoch >
+                      b.originServerTs.millisecondsSinceEpoch
+                  ? a
+                  : b;
+            });
+
+      print(
+          'lastEventAvailableInPreview datph at 1 room: ${getLocalizedDisplayname(MatrixLocals(L10n.of(context)!))} lastEvent : ${jsonEncode(lastEventAvailableInPreview)}');
+
+      if (lastEventAvailableInPreview == null ||
+          (AppConfig.hideRedactedEvents && lastEventAvailableInPreview.redacted)) {
+        final lastState = _getLastestRoomState();
+
+        if (lastState == null) return null;
+
+        lastEventAvailableInPreview = lastState;
+        final messageEvents =
+            await client.database?.getEventList(this, limit: 10);
+        if (messageEvents == null || messageEvents.isEmpty) {
+          return lastState;
+        }
+
+        for (final messageEvent in messageEvents) {
+          // print(
+          //     'lastEventAvailableInPreview datph at room: ${getLocalizedDisplayname(
+          //   MatrixLocals(L10n.of(context)!),
+          // )} messageEvent time: ${messageEvent.originServerTs} - lastState time: ${lastState.originServerTs}');
+
+          // print(
+          //     'lastEventAvailableInPreview datph at room: ${getLocalizedDisplayname(
+          //   MatrixLocals(L10n.of(context)!),
+          // )} should hide redact ${_shouldHideRedactedEvents(messageEvent)}');
+
+          print(
+              'lastEventAvailableInPreview datph at room: ${getLocalizedDisplayname(
+            MatrixLocals(L10n.of(context)!),
+          )} config hide ${AppConfig.hideRedactedEvents} - message redacted: ${messageEvent.redacted} - message: ${jsonEncode(messageEvent)}');
+          if (_shouldHideRedactedEvents(messageEvent)) continue;
+
+          if (messageEvent.originServerTs.millisecondsSinceEpoch >
+              lastState.originServerTs.millisecondsSinceEpoch) {
+            print(
+                'lastEventAvailableInPreview datph at room: ${getLocalizedDisplayname(
+              MatrixLocals(L10n.of(context)!),
+            )} datph change ');
+            lastEventAvailableInPreview = messageEvent;
+            break;
+          }
+        }
+      }
+    } catch (e) {
+      Logs().e('Room::lastEventAvailableInPreview: room: $id error - $e');
+      return null;
+    }
+
+    print('lastEventAvailableInPreview datph at room: ${getLocalizedDisplayname(
+      MatrixLocals(L10n.of(context)!),
+    )} last EVENT = ${jsonEncode(lastEventAvailableInPreview)}');
+    return lastEventAvailableInPreview;
+  }
+
+  bool _shouldHideRedactedEvents(Event event) {
+    return AppConfig.hideRedactedEvents && event.redacted;
+  }
+
+  Event? _getLastestRoomState() {
+    var lastTime = DateTime.fromMillisecondsSinceEpoch(0);
+    Event? lastState;
+    states.forEach((final String key, final entry) {
+      final state = entry[''];
+      if (state == null) return;
+      if (AppConfig.hideRedactedEvents && state.redacted) return;
+      if (state.originServerTs.millisecondsSinceEpoch >
+          lastTime.millisecondsSinceEpoch) {
+        lastTime = state.originServerTs;
+        lastState = state;
+      }
+    });
+    return lastState;
   }
 }
